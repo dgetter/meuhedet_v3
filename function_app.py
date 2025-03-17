923767700c369e5f3519a331e481416e9f569ce8
dgetter <dgetter@kpmg.com>
2025-03-17 11:37:17 +0200

    fastapi wrapper

--- function_app.py
+++ function_app.py
@@ -1,18 +1,5 @@
 import azure.functions as func
-from fastapi import FastAPI
-import logging
 
-# Create FastAPI app
-app = FastAPI()
+from WrapperFunction import app as fastapi_app
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI in Azure Functions"}
-
-@app.post("/classifier_endpoint")
-def classifier_endpoint(data: dict):
-    name = data.get("name", "Guest")
-    return {"message": f"Hello, {name}. This HTTP triggered function executed successfully."}
-
-# Wrap FastAPI app in Azure Functions
-function_app = func.AsgiFunctionApp(app=app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
